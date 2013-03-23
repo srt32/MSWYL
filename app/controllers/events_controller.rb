2d93111cabd5459c78a5c7dc0a3f9dcc6f214815
Simon <simon.taranto@gmail.com>
2013-03-23 09:51:36 -0600

    restricting non-admin user access to events#new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,6 +30,7 @@ class EventsController < ApplicationController
   # GET /events/new.json
   def new
     @event = Event.new
+    authorize! :new, @event
 
     respond_to do |format|
       format.html # new.html.erb
