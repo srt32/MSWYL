fa6b16216208c8f99e644f9b02d8ed6a6d4f16bc
Simon <simon.taranto@gmail.com>
2013-03-23 12:51:23 -0600

    force precomile of *.css in application.rb

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module TMS1
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.precompile += %w[*.css] 
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
