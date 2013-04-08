89228b751098d39e67d503fb26dbb8799dcc4bed
Simon <simon.taranto@gmail.com>
2013-04-07 22:04:12 -0600

    limit num guests to be between 0 and 30

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -2,4 +2,9 @@ class Attendee < ActiveRecord::Base
   attr_accessible :email, :event_id, :name, :num_guests, :semester
 
   belongs_to :event
+
+  validates_numericality_of :num_guests, :only_integer => true, 
+  								:message => "can only be whole number."
+  validates_inclusion_of :num_guests, :in => 0..30, 
+  								:message => "can only be between 0 and 30."
 end
