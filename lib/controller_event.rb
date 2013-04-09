f914c611505769e8f704b781a4f2331f463abe8c
Simon <simon.taranto@gmail.com>
2013-04-08 20:49:35 -0600

    add functionality to have total RSVP's

--- lib/controller_event.rb
+++ lib/controller_event.rb
@@ -1,10 +1,13 @@
 module ControllerEvent
   def self.included(controller)
-    controller.send :helper_method, :current_event
+    controller.send :helper_method, :current_event, :total_rsvps
   end
 
   def current_event
   	@current_event = Event.find_by_id(params[:event_id])
   end
 
+  def total_rsvps
+  	Attendee.all.count + Attendee.sum("num_guests")
+  end
 end
