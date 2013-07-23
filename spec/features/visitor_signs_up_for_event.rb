6fc0f4a3a0bcc921777b315e74be6099b991bb6d
Simon <simon.taranto@gmail.com>
2013-07-23 09:00:53 -0600

    remove old test dirs

--- spec/features/visitor_signs_up_for_event.rb
+++ spec/features/visitor_signs_up_for_event.rb
@@ -6,7 +6,7 @@ describe 'Visitor signs up for event', :type => :feature do
       visit "/"
     end
 
-  describe 'get /' do
+  describe 'visitor signing up for event' do
   	it "should display 'Eat mushrooms'" do
 	    page.should have_content("Eat mushrooms")
 		end
@@ -14,6 +14,15 @@ describe 'Visitor signs up for event', :type => :feature do
     it "should go to event#show page when user clicks on event" do
       click_link 'See more details'
       page.should have_content("Event Name: Eat mushrooms")
+    end
+
+    it "can sign up for an event" do
+      pending "user can sign up"
+      # click SIGN ME UP
+      # fill out form
+      # submit form
+      # should go to some page...
+      # this visitor should be included in the guest list
     end
   end
 end
